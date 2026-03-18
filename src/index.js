bef5431cf48f48d97114e772ee6f6638a87ded03
RaunakAnand <142423835+RaunakAnand@users.noreply.github.com>
2026-03-19 00:23:30 +0530

    feat: added Supabase Google authentication

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AppProvider } from './context/AppContext';
+import { GoogleOAuthProvider } from '@react-oauth/google'; // 1. Import the provider
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 2. Grab your Client ID from the .env file
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
 root.render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    {/* 3. Wrap your AppProvider with the GoogleOAuthProvider */}
+    <GoogleOAuthProvider clientId={clientId}>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
